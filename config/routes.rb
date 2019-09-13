41e585abbdf29bb3dcbdbf72bfe6757acd282eb6
mathieu bouchant <matbcht@gmail.com>
2019-09-13 10:42:22 +0200

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+
+  get 'restaurants', to: 'restaurants#index'
+
+
+  root to:'pages#home'
+
+
+  # Generic syntax:
+  # verb 'path', to: 'controller#action', as: :route_name
 end
